7d3ce1f74e464fdd3620bc0aad9b754b92e3e6d1
linonetwo <linonetwo012@gmail.com>
2019-04-11 00:02:43 +0800

    Initial Commit

---
+++ src/block-casual-user/index.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name                自动屏蔽知乎三无用户的评论
+// @description:zh-CN   自动屏蔽无头像、无贡献、不友善用户的评论（目前仅实现头像检测，贡献检测太耗流量、基于 SoLiD 的云黑名单还在实验中）
+// @namespace           https://onetwo.ren/
+// @match               *://*.zhihu.com/*
+// @grant none
+// @version             0.1
+// @homepageURL         https://onetwo.ren/
+// @supportURL          https://github.com/linonetwo
+// @license             MIT
+// @icon                https://pic1.zhimg.com/2e33f063f1bd9221df967219167b5de0_m.jpg
+// @run-at 		        document-start
+// @date                04/10/2019
+// @modified            04/10/2019
+// ==/UserScript==
+
+const userscriptautocollapsed = 'userscriptautocollapsed';
+const 默认头像的URL = 'https://pic4.zhimg.com/da8e974dc_s.jpg';
+const 检查间隔毫秒 = 250;
+
+const 给评论列表加上事件监听器 = () => {
+    // 轮询直到评论列表加载完毕
+    const intervalHandler = setInterval(() => {
+        const 评论列表 = document.querySelector('div.CommentListV2');
+        if (评论列表) {
+            clearInterval(intervalHandler);
+            自动隐藏没有头像的用户的评论();
+            评论列表.addEventListener('scroll', throttle(自动隐藏没有头像的用户的评论, 检查间隔毫秒));
+        }
+    }, 检查间隔毫秒);
+}
+
+function 给评论按钮加上事件监听器以便给评论列表加上事件监听器() {
+  [...document.querySelectorAll('button.Button.ContentItem-action.Button--plain.Button--withIcon.Button--withLabel')] // 知乎的图片放在 figure 里面
+    .filter(按钮 => 按钮.innerText.includes('评论'))
+    .forEach(评论按钮 => 评论按钮.addEventListener('click', 给评论列表加上事件监听器, { passive : true }));
+};
+
+function 自动隐藏没有头像的用户的评论() {
+  [...document.querySelectorAll('ul.NestComment')] // 选择每条评论
+    .filter(评论 => {
+      if (评论.dataset[userscriptautocollapsed]) return false;
+
+      const 头像 = 评论.querySelector('img.Avatar');
+      if (头像 && 头像.src === 默认头像的URL) return true;
+    })
+    .forEach(评论 => {
+      评论.dataset[userscriptautocollapsed] = true;
+      评论.style = "display: none;";
+    });
+};
+
+
+// 知乎会延迟加载内容，所以当滚动页面的时候再检查一次
+// From: https://www.sitepoint.com/throttle-scroll-events/
+function throttle(fn, wait) {
+  var time = Date.now();
+  return function() {
+    if ((time + wait - Date.now()) < 0) {
+      fn();
+      time = Date.now();
+    }
+  }
+}
+
+给评论按钮加上事件监听器以便给评论列表加上事件监听器();
+setTimeout(() => window.addEventListener('scroll', throttle(给评论按钮加上事件监听器以便给评论列表加上事件监听器, 检查间隔毫秒), { passive : true }), 1);
---
+++ src/collapse-mobile-screenshot-answer/index.js
@@ -0,0 +1,72 @@
+// ==UserScript==
+// @name                自动收起含有手机截图的回答
+// @description:zh-CN   自动收起含有手机截图的回答
+// @namespace           https://onetwo.ren/
+// @match               *://*.zhihu.com/*
+// @exclude             *://*.zhihu.com/question/*/answer/*
+// @grant none
+// @version             0.1
+// @homepageURL         https://onetwo.ren/
+// @supportURL          https://github.com/linonetwo
+// @license             MIT
+// @icon                https://pic1.zhimg.com/2e33f063f1bd9221df967219167b5de0_m.jpg
+// @run-at 		        document-start
+// @date                04/10/2019
+// @modified            04/10/2019
+// ==/UserScript==
+const userscriptautocollapsed = 'userscriptautocollapsed';
+const 常见手机纵横比 = 16 / 9;
+const 检查间隔毫秒 = 200;
+
+function autoFoldAnswerWithMobileScreenShot() {
+  const 收起按钮们 = [...document.querySelectorAll('figure > img')] // 知乎的图片放在 figure 里面
+    .filter(img => {
+      const height = Number(img.getAttribute('data-rawheight'));
+      const width = Number(img.getAttribute('data-rawwidth'));
+      return height && width && height > width * (常见手机纵横比);
+    })
+    .map(img => img.closest('.RichContent')) // 获取回答框
+    .filter(回答框 => !回答框.dataset[userscriptautocollapsed]) // 不让它们被收起第二次
+    .map(回答框 => {
+      // 获取按钮并点击
+      const 答案底部按钮们 = 回答框.querySelectorAll('div.ContentItem-actions > button')
+      const 收起按钮 = 答案底部按钮们[答案底部按钮们.length - 1];
+      return 收起按钮;
+    })
+    .filter(收起按钮 => 收起按钮.innerText.includes('收起'));
+
+  if (收起按钮们.length > 0) {
+    // 准备好标记这些要被收起的框框，不让它们被收起第二次，如果你手动展开了它们的话
+    const 收起的回答框们 = 收起按钮们
+      .map(按钮 => 按钮.closest('.RichContent'))
+      .forEach(回答框 => { 回答框.dataset[userscriptautocollapsed] = true });
+    // 点击收起按钮
+    收起按钮们.forEach(按钮 => 按钮.click());
+    console.log(`有 ${收起按钮们.length} 个带有长图的答案需要收起`);
+    console.log(`在电脑端自动收起所有纵横比大于 ${常见手机纵横比} 的 图片`);
+  }
+};
+
+autoFoldAnswerWithMobileScreenShot();
+
+
+
+// 知乎会延迟加载内容，所以在鼠标主动移动（用滚轮不算）到个人简介栏的时候再检查一次
+// function addEventListenerToAnswerHeader() {
+//   [...document.querySelectorAll('div.AuthorInfo')]
+//   .forEach(用户信息栏 => 用户信息栏.addEventListener('mouseover', autoFoldAnswerWithMobileScreenShot, { passive : true }));
+// }
+
+
+// 知乎会延迟加载内容，所以当滚动页面的时候再检查一次
+// From: https://www.sitepoint.com/throttle-scroll-events/
+function throttle(fn, wait) {
+  var time = Date.now();
+  return function() {
+    if ((time + wait - Date.now()) < 0) {
+      fn();
+      time = Date.now();
+    }
+  }
+}
+setTimeout(() => window.addEventListener('scroll', throttle(autoFoldAnswerWithMobileScreenShot, 检查间隔毫秒), { passive : true }), 1);
