bd7f0149ed392ae82a8b22b08c8a604ae1607b71
lin onetwo <linonetwo012@gmail.com>
2019-05-24 13:51:03 +0800

    Create skip-tampermonkey-page.user.js

---
+++ src/skip-tampermonkey-page.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @author        Linonetwo
+// @name          Skip tampermonkey page
+// @namespace     https://github.com/linonetwo
+// @description   Automatically redirects tampermonkey page to openuserjs.
+// @version       1.0
+// @grant         none
+// @run-at        document-start
+// @include       https://www.tampermonkey.net/scripts.php
+// @icon          https://www.tampermonkey.net/images/icon.png
+// ==/UserScript==
+
+const url = window.location.href;
+const regex = /^https?:\/\/(?:www\.)?tampermonkey\.net\/scripts\.php/i;
+const match = url.match(regex);
+
+if (match) {
+    window.location.href = 'https://openuserjs.org/';
+}
